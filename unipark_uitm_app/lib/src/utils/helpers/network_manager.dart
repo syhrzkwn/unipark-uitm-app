68847169c0eba86e8a21c90f3c137d1d262d2ab5
syhrzkwn <syahirzakwan.dev@gmail.com>
2023-12-12 03:27:53 +0800

    Create a network manager function

---
+++ unipark_uitm_app/lib/src/utils/helpers/network_manager.dart
@@ -0,0 +1,49 @@
+import 'dart:async';
+import 'package:connectivity_plus/connectivity_plus.dart';
+import 'package:flutter/services.dart';
+import 'package:get/get.dart';
+import 'package:unipark_uitm_app/src/utils/theme/widget_themes/snackbar_theme.dart';
+
+class NetworkManager extends GetxController {
+  static NetworkManager get instance => Get.find();
+
+  final Connectivity _connectivity = Connectivity();
+  late StreamSubscription<ConnectivityResult> _connectivitySubscription;
+  final Rx<ConnectivityResult> _connectionStatus = ConnectivityResult.none.obs;
+
+  @override
+  void onInit() {
+    super.onInit();
+    _connectivitySubscription = _connectivity.onConnectivityChanged.listen(_updateConnectionStatus);
+  }
+
+  // Update the connection status based on changes in connectivity and display a relevant snackbar for no internet connection
+  Future<void> _updateConnectionStatus(ConnectivityResult result) async {
+    _connectionStatus.value = result;
+    if (_connectionStatus.value == ConnectivityResult.none) {
+      SnackBarTheme.warningSnackBar(title: "Warning", message: "The screen isn't loading. Please check your network connection.");
+    }
+  }
+
+  // Check the internet connection status
+  // Return true if connected, false otherwise
+  Future<bool> isConnected() async {
+    try {
+      final result = await _connectivity.checkConnectivity();
+      if (result == ConnectivityResult.none) {
+        return false;
+      } else {
+        return true;
+      }
+    } on PlatformException catch (_) {
+      return false;
+    }
+  }
+
+  // Dispose or close the active connectivity stream
+  @override
+  void onClose() {
+    super.onClose();
+    _connectivitySubscription.cancel();
+  }
+}
