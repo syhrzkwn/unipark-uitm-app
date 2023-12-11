76dafbbdbc5fd2fe5a5d37ede661f88f062c2486
syhrzkwn <syahirzakwan.dev@gmail.com>
2023-12-12 03:18:02 +0800

    Reorganised the folder structure

--- unipark_uitm_app/lib/src/constants/images.dart
+++
@@ -1,2 +0,0 @@
-const String uniparkLogo = "assets/images/unipark_logo.png";
-const String googleLogo = "assets/images/google_logo.png";
--- unipark_uitm_app/lib/src/constants/sizes.dart
+++
@@ -1,3 +0,0 @@
-const defaultSize = 25.0;
-const buttonHeight = 20.0;
-const formHeight = 30.0;
