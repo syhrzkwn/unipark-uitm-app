d1163621d25ff4106f9c9c13805d472b359413a7
syhrzkwn <syahirzakwan.dev@gmail.com>
2023-12-12 03:27:09 +0800

    Create a formatted for date and phone number

---
+++ unipark_uitm_app/lib/src/utils/theme/formatter/formatter.dart
@@ -0,0 +1,18 @@
+import 'package:intl/intl.dart';
+
+class Formatter {
+  static String formatDate(DateTime? date) {
+    date ??= DateTime.now();
+    return DateFormat('dd-MM-yyyy').format(date);
+  }
+
+  // Format phone number, example: +6012-3456789
+  static String formatPhoneNumber(String phoneNumber) {
+    if(phoneNumber.length == 10) {
+      return '+6${phoneNumber.substring(0, 1)} ${phoneNumber.substring(1, 3)}-${phoneNumber.substring(3, 10)}';
+    } else if(phoneNumber.length == 10) {
+      return '+6${phoneNumber.substring(0, 1)} ${phoneNumber.substring(1, 3)}-${phoneNumber.substring(3, 11)}';
+    }
+    return phoneNumber;
+  }
+}
