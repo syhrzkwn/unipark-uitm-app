59fe0773cd0f33d6fa1cd9b014e8009e773cfdbc
syhrzkwn <syahirzakwan.dev@gmail.com>
2023-12-17 21:53:03 +0800

    Update user repository for User CRUD

--- unipark_uitm_app/lib/src/data/repositories/user/user_repository.dart
+++ unipark_uitm_app/lib/src/data/repositories/user/user_repository.dart
@@ -1,20 +1,71 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+// import 'package:firebase_database/firebase_database.dart'; // Firebase Realtime DB
 import 'package:get/get.dart';
+import 'package:unipark_uitm_app/src/data/repositories/authentication/authentication_repository.dart';
 import 'package:unipark_uitm_app/src/features/core/models/user_model.dart';
 import 'package:unipark_uitm_app/src/data/repositories/exceptions/firebase_auth_exception.dart';
 
 class UserRepository extends GetxController {
   static UserRepository get instance => Get.find();
-  
-  final FirebaseFirestore _db = FirebaseFirestore.instance;
-  
+
+  // Firebase Firestore DB
+  final FirebaseFirestore _dbFirestore = FirebaseFirestore.instance;
+  // Firebase Realtime DB
+  // final FirebaseDatabase _dbRealTime = FirebaseDatabase.instance;
+
   Future<void> saveUserRecord(UserModel user) async {
     try {
-      await _db.collection("Users").doc(user.id).set(user.toJson());
+      await _dbFirestore.collection("Users").doc(user.id).set(user.toJson());
+      // await _dbRealTime.ref().child("Users").child(user.id).set(user.toJson());
     } on FirebaseException catch (e) {
       throw TFirebaseAuthException(e.code).message;
     } catch (e) {
-      throw 'Something went wrong. Please try again';
+      throw 'Something went wrong. Please try again.';
+    }
+  }
+
+  Future<UserModel> fetchUserInformation() async {
+    try {
+      final documentSnapshot = await _dbFirestore.collection("Users").doc(AuthenticationRepository.instance.authUser?.uid).get();
+      if (documentSnapshot.exists) {
+        return UserModel.fromSnapshot(documentSnapshot);
+      } else {
+        return UserModel.empty();
+      }
+    } on FirebaseException catch (e) {
+      throw TFirebaseAuthException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again.';
+    }
+  }
+
+  Future<void> updateUserInformation(UserModel updateUser) async {
+    try {
+      await _dbFirestore.collection("Users").doc(updateUser.id).update(updateUser.toJson());
+    } on FirebaseException catch (e) {
+      throw TFirebaseAuthException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again.';
+    }
+  }
+
+  Future<void> updateSingleField(Map<String, dynamic> json) async {
+    try {
+      await _dbFirestore.collection("Users").doc(AuthenticationRepository.instance.authUser?.uid).update(json);
+    } on FirebaseException catch (e) {
+      throw TFirebaseAuthException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again.';
+    }
+  }
+
+  Future<void> removeUserRecord(String userId) async {
+    try {
+      await _dbFirestore.collection("Users").doc(userId).delete();
+    } on FirebaseException catch (e) {
+      throw TFirebaseAuthException(e.code).message;
+    } catch (e) {
+      throw 'Something went wrong. Please try again.';
     }
   }
 }
