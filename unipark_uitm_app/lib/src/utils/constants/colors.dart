cc41f2d34b7ac14a8a2201e5812269270cd00a16
syhrzkwn <syahirzakwan.dev@gmail.com>
2023-12-29 16:19:35 +0800

    Add a few constant colors

--- unipark_uitm_app/lib/src/utils/constants/colors.dart
+++ unipark_uitm_app/lib/src/utils/constants/colors.dart
@@ -8,3 +8,7 @@ const darkModeBackground = Color(0xFF303030);
 const textColor1 = Color(0xFF454545);
 const textColor2 = Color(0xFF767676);
 const borderColor = Color(0xFFE0E0E0);
+const activeGreenText = Color(0xFF50C878);
+const activeGreen = Color(0x3650C878);
+const redColor = Color(0xFFF44336);
+const redColorBackground = Color(0x36F44336);
