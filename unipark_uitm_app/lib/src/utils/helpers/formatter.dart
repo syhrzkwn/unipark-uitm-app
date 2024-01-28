f32961b041bc0826cb163767904922bc937c1ce1
syhrzkwn <syahirzakwan.dev@gmail.com>
2024-01-29 06:40:26 +0800

    Create a formatter for date time

--- unipark_uitm_app/lib/src/utils/helpers/formatter.dart
+++ unipark_uitm_app/lib/src/utils/helpers/formatter.dart
@@ -6,6 +6,11 @@ class Formatter {
     return DateFormat('yyyy-MM-dd').format(date);
   }
 
+  static String formatDateTime(DateTime? date) {
+    date ??= DateTime.now();
+    return DateFormat('dd MMM yyyy, H:mm a').format(date);
+  }
+
   // Format phone number, example: +6012-3456789
   static String formatPhoneNumber(String phoneNumber) {
     if(phoneNumber.length == 10) {
